724d58377667a24191c5049af28a323daa522362
Ricardo Argudin <ricardo1.argudin@gmail.com>
2018-02-06 21:04:31 -0600

    Added html Template

--- website/music/views.py
+++ website/music/views.py
@@ -5,12 +5,11 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template(' ')
-    html = ''
-    for album in all_albums:
-        url = '/music/' + str(album.id) + '/'
-        html += '<a href="'+ url + '">' + album.album_title + '</a><br>'
-    return HttpResponse(html)
+    template = loader.get_template('music/index.html')
+    context = {
+        'all_albums': all_albums,
+    }
+    return HttpResponse(template.render(context, request))
 
 
 def detail(request, album_id):
